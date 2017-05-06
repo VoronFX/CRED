b693e26bee59d7aa753b33429de906dfc420bc82
Voronin Igor <voron.exe@gmail.com>
2017-05-06 11:44:32 +0900

    Berfore some more rework

--- Utilities/AzurePortalExtractor/Form1.cs
+++ Utilities/AzurePortalExtractor/Form1.cs
@@ -77,145 +77,85 @@ namespace AzurePortalExtractor
 			}
 		}
 
+		static Uri currentUri = new Uri("http://portal.azure.com/");
+
+		(string downlodHref,
+		string replaceHref,
+		string sourcePath,
+		string targetPath,
+		string extension)[] overrides = new[]
+			{
+				(currentUri + "../fonts/segoe-ui/cyrillic/normal/latest.eot",
+				"../fonts/segoe-ui/cyrillic/normal/latest.eot",
+				"Overrides/segoe-ui_cyrillic_normal_latest.eot",
+				"Fonts/segoe-ui/cyrillic/normal/latest", ".eot"),
+
+				(currentUri + "../fonts/segoe-ui/cyrillic/normal/latest.eot",
+				"../fonts/segoe-ui/cyrillic/normal/latest.eot",
+				"Overrides/segoe-ui_cyrillic_normal_latest.eot",
+				"LinkHrefStylesheet", ".css"),
+			};
+
+
 		private void ExtractFromAzureSourcesBtn_Click(object sender, EventArgs e)
 		{
 			try
 			{
+				var SourceDirectory = textBox1.Text;
+				var WwwrootDirectory = textBox2.Text;
+				var RelativeOutputDirectory = textBox3.Text;
+				var OutputDirectory = Path.Combine(WwwrootDirectory, RelativeOutputDirectory);
+
 				//var _Content_Dynamic_UxFxStableCssCyrillic_C7498CEC16966F59B995A5ED9691B131213D24B7_css_c1_ru_c2_ru_ "df";
 				//Viva_Controls__Controls__Visualization__Viva_Map_css = Viva_Controls__Controls__Visualization__Viva_Map_css;
-				Directory.GetFiles(textBox1.Text, "*.html")
+				var parsedResources = Directory.GetFiles(textBox1.Text, "*.html")
 					.Select(File.ReadAllText)
 					.Select(x =>
 					{
 						var head = Regex.Match(x, "(?i)(?<=<head>).*?(?=</head>)").Value;
 						var takenIdentifiers = new HashSet<string>();
 
-						Regex.Matches(head, "<(?:style|svg)[^<>]*?>.*?</(?:style|svg)>|<link[^<>]*?>")
+						return Regex.Matches(head, "<(?:style|svg)[^<>]*?>.*?</(?:style|svg)>|<link[^<>]*?type=\"text/css\"[^<>]*?>")
 							.Cast<Match>()
-							.SelectMany(el =>
-							{
+							.Select(el => ParseAzurePage(el, takenIdentifiers, OutputDirectory));
 
-								var Identifier = string.Empty;
-								var Path = string.Empty;
-								var Content = string.Empty;
-								var Comment = string.Empty;
+					}).ToArray();
 
-								//var content =>
-								//var contentR = Regex.Match(el.Value, "(?i)(?<=<[^<>]*?>).*(?=<[^<>]*?>)").Value;
-								//if (contentMatch s)
-								var tag = Regex.Match(el.Value, "(?i)(?<=<)\\w+?(?=\\s+?)(?=[^<]*?>)").Value.ToLowerInvariant();
+				foreach (var valueTuple in parsedResources[0])
+				{
+					
+				}
 
-								string attr(string tagName, string attrName) =>
-									Regex.Match(el.Value, $"(?i)(?<=<{tagName}\\s+?[^<>]*?{attrName}=\").*?(?=\"[^<]*?>)").Value;
+				var resDefinitions = new List<(string)>();
 
-								//string pathBase(string path) => Regex.Match(path, ".*?/").Value;
-								//string toIdentifier(string text) => Regex.Replace(text, "[^A-Za-z]", "_");
-								//string toPath(string text) => Regex.Replace(text, @"[^A-Za-z\.\\/]", "_");
+				foreach (var valueTuple in parsedResources[0])
+				{
+					foreach (Match match in 
+						Regex.Matches(valueTuple.Content, @"(?i)(?<=url\(').+?(?='\))")
+						.Cast<Match>())
+					{
+						var resOverride = overrides.First(ov => ov.replaceHref == match.Value);
 
-								void setIdentifier(string path, string ext)
-								{
-									Path = Regex.Replace(uniqaize(path) + ext, @"[^A-Za-z\.\\/]", "_");
-									Identifier = Regex.Replace(Path, "[^A-Za-z]", "_");
-								}
-
-								string uniqaize(string ident)
-								{
-									int i = 1;
-									string result = ident;
-									while (takenIdentifiers.Contains(result))
-									{
-										result = $"{ident}_{i}";
-										i++;
-									}
-									takenIdentifiers.Add(result);
-									return result;
-								}
-
-								string parseDependency(string text)
-								{
-									
-								}
-								//var TagMatcherPattern = "(?i)<(?<tag>\\w+?)(\\s|>)[^<>]*?"
-								//                        + string.Join("", new[] {"type", "class", "href", "data-extensionwindowid", "rel", "property" }
-								//						.Select(t => $"(?:{t}=\"(?<{t}>.*?)\")?[^<>]*?\n"));
-								//"(?i)<link[^<>]*?type=\"text/css\"[^<>]*?href=\"(?<href>.*?)\"[^<>]*?>"
-
-								Content = Regex.Match(el.Value, "(?i)(?<=<[^<>]*?>).*(?=<[^<>]*?>)").Value;
-								var currentUri = new Uri("http://portal");
-								switch (tag)
-								{
-									case "link":
-
-										if (attr("link", "type") != "text/css")
-											return;
-
-										var href = attr("link", "href");
-
-										var uri = new Uri(href, UriKind.RelativeOrAbsolute);
-										if (!uri.IsAbsoluteUri)
-											uri = currentUri.MakeRelativeUri(uri);
-										currentUri = uri;
-
-										Content = new HttpClient().GetStringAsync(currentUri).Result;
-
-
-
-										Comment = attr("link", "href");
-										setIdentifier("LinkHrefStylesheet", ".css");
-
-										break;
-
-									case "style":
-
-										Identifier = attr("style", "class");
-
-										if (string.IsNullOrWhiteSpace(Identifier))
-											Identifier = "ExtensionWindow/" + attr("style", "data-extensionwindowid");
-
-										if (string.IsNullOrWhiteSpace(Identifier))
-											Identifier = "UnnamedStylesheet";
-
-										Identifier = Regex.Replace(Identifier, "(?i)\\.css$", "");
-
-										setIdentifier(Identifier, ".css");
-
-										break;
-
-									case "svg":
-
-										Identifier = attr("symbol", "id");
-										if (string.IsNullOrWhiteSpace(Identifier))
-											return;
-
-										setIdentifier("Svg/" + Identifier, ".svg");
-
-										break;
-									default:
-										throw new Exception("Parse exception! Uknown element matched");
-								}
-
-
-
-								//return new
-								//{
-								//	Identifier = "s",
-								//	Path = "fas",
-								//	Content = "sda"
-								//	Comment = "sda"
-								//}
-							});
-
-
-						return new
+						var linkOverrideFilePath = Path.Combine(OutputDirectory, resOverride.sourcePath);
+						if (File.Exists(linkOverrideFilePath))
 						{
-							Styles = Regex.Matches(
-						Regex.Match(x, "(?i)(?<=<div\\s+?class(=[\"'][\"']>)?).*?(?=</div>)").Value,
-						"(?<=<style\\s+?class=[\"\'](?<name>[^\"\']+?)[\"\']>).*?(?=</style>)").Cast<Match>()
-						.Select(x => new { Name = x.Groups["name"], x.Value }),
-
-							Symbols = Regex.Match()
+							Content = File.ReadAllBytes(linkOverrideFilePath);
 						}
-					})
+						else
+						{
+							var uri = new Uri(href, UriKind.RelativeOrAbsolute);
+							if (!uri.IsAbsoluteUri)
+								uri = currentUri.MakeRelativeUri(uri);
+
+							Content = new HttpClient().GetStringAsync(uri).Result;
+							File.WriteAllText(linkOverrideFilePath, Content);
+						}
+
+					}
+				}
+
+				var missingRes = parsedResources[0].SelectMany(x=> Regex.Matches(x.Content, @"(?i)(?<=url\(').+?(?='\))")).Cast<>()
+
 
 				foreach (string file in Directory.GetFiles(textBox1.Text, "*.html").Select(File.ReadAllText))
 				{
@@ -226,6 +166,113 @@ namespace AzurePortalExtractor
 			{
 				MessageBox.Show(exception.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
 			}
+		}
+
+		private (string Identifier, string TargetPath, string Content, string Comment) 
+			ParseAzurePage(Match el, HashSet<string> takenIdentifiers, string OutputDirectory)
+		{
+			string Content = Regex.Match(el.Value, "(?i)(?<=<[^<>]*?>).*(?=<[^<>]*?>)").Value;
+			string Identifier;
+			string TargetPath;
+			string Comment;
+
+			var tag = Regex.Match(el.Value, "(?i)(?<=<)\\w+?(?=\\s+?)(?=[^<]*?>)").Value.ToLowerInvariant();
+
+			string attr(string tagName, string attrName) =>
+				Regex.Match(el.Value, $"(?i)(?<=<{tagName}\\s+?[^<>]*?{attrName}=\").*?(?=\"[^<]*?>)").Value;
+
+			void setIdentifier(string path, string ext)
+			{
+				TargetPath = Regex.Replace(uniqaize(path) + ext, @"[^A-Za-z\.\\/]", "_");
+				Identifier = Regex.Replace(TargetPath, "[^A-Za-z]", "_");
+			}
+
+			string uniqaize(string ident)
+			{
+				int i = 1;
+				string result = ident;
+				while (takenIdentifiers.Contains(result))
+				{
+					result = $"{ident}_{i}";
+					i++;
+				}
+				takenIdentifiers.Add(result);
+				return result;
+			}
+
+
+			switch (tag)
+			{
+				case "link":
+
+					var href = attr("link", "href");
+
+					var linkOverride = overrides.First(ov => ov.replaceHref == href);
+
+					var linkOverrideFilePath = Path.Combine(OutputDirectory, linkOverride.sourcePath);
+					if (File.Exists(linkOverrideFilePath))
+					{
+						Content = File.ReadAllText(linkOverrideFilePath);
+					}
+					else
+					{
+						var uri = new Uri(href, UriKind.RelativeOrAbsolute);
+						if (!uri.IsAbsoluteUri)
+							uri = currentUri.MakeRelativeUri(uri);
+
+						Content = new HttpClient().GetStringAsync(uri).Result;
+						File.WriteAllText(linkOverrideFilePath, Content);
+					}
+
+					Comment = attr("link", "href");
+					setIdentifier(linkOverride.targetPath, linkOverride.extension);
+
+					break;
+
+				case "style":
+
+					var extensionwindowid = attr("style", "data-extensionwindowid");
+					var styleClass = attr("style", "class");
+
+					if (!string.IsNullOrWhiteSpace(extensionwindowid))
+					{
+						Identifier = "ExtensionWindow/" + extensionwindowid;
+						Comment = extensionwindowid;
+					}
+					else if (!string.IsNullOrWhiteSpace(styleClass))
+					{
+						Identifier = styleClass;
+						Comment = styleClass;
+					}
+					else
+					{
+						Identifier = "UnnamedStylesheet";
+						Comment = "Style hadn't any identifier";
+					}
+
+					Identifier = Regex.Replace(Identifier, "(?i)\\.css$", "");
+
+					setIdentifier(Identifier, ".css");
+
+					break;
+
+				case "svg":
+
+					Identifier = attr("symbol", "id");
+
+					if (string.IsNullOrWhiteSpace(Identifier))
+						throw new Exception("Svg symbol id not found");
+
+					Comment = Identifier;
+
+					setIdentifier("Svg/" + Identifier, ".svg");
+
+					break;
+				default:
+					throw new Exception("Parse exception! Uknown element matched");
+			}
+
+			return (Identifier, TargetPath, Content, Comment);
 		}
 	}
 }
