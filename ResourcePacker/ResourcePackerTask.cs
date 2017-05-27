15001ea9fd9b0207145ee124720c4eaab94b080d
Voronin Igor <voron.exe@gmail.com>
2017-05-28 07:36:02 +0900

    Created working non locking msbuild task

---
+++ ResourcePacker/ResourcePackerTask.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Resources;
+using System.Text;
+using Microsoft.Build.Framework;
+using Microsoft.Build.Utilities;
+using Mono.Cecil;
+
+namespace ResourcePacker
+{
+	public class ResourcePackerTask : Task
+	{
+		[Required]
+		public string IntermediateAssembly { get; set; }
+
+		public override bool Execute()
+		{
+			//var module = ModuleDefinition.ReadModule(IntermediateAssembly);
+			//foreach (var attribute in module.Types
+			//	.Where(x => x.HasCustomAttributes)
+			//	.SelectMany(x => x.CustomAttributes)
+			//	.Where(x => x.AttributeType.FullName == typeof(RequireResourceAttribute).FullName
+			//	            && x.HasConstructorArguments)
+			//	.Select(x => x.ConstructorArguments.First())
+			//	.Where(x => x.Type.FullName == typeof(string).FullName))
+			//{
+			//	Console.WriteLine(attribute.Value);
+			//	//module.Resources.Add(new EmbeddedResource("", ManifestResourceAttributes.Public, ));
+			//	//attribute.ConstructorArguments[0].
+			//	//foreach (CustomAttribute attribute in type.CustomAttributes)
+			//	//{
+			//	//	if (attribute.AttributeType.FullName != attributeType)
+			//	//		continue;
+
+			//	//	result = attribute;
+			//	//	return true;
+			//	//}
+
+			//	//return false;
+
+			//	//Console.WriteLine(type.FullName);
+			//}
+
+
+
+
+			//Log.LogError("Hello, wsorld!");
+			//File.Delete(IntermediateAssembly);
+			//var assembly = Assembly.ReflectionOnlyLoadFrom(IntermediateAssembly);
+			Log.LogError("aaaaaas2adad");
+			return false;
+		}
+	}
+}
---
+++ ResourcePacker/ResourcePackerTaskWrapper.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+using Microsoft.Build.Utilities;
+using Microsoft.Build.Framework;
+
+namespace ResourcePacker
+{
+	public class ResourcePackerTaskWrapper : Task
+	{
+		[Required]
+		public string MSBuildThisFileDirectory { get; set; }
+
+		public override bool Execute()
+		{
+			var path = MSBuildThisFileDirectory + @"\bin\Debug\";
+			var assembly = File.Exists(path + "ResourcePacker.pdb") ? Assembly.Load(
+				  File.ReadAllBytes(path + "ResourcePacker.dll"),
+				  File.ReadAllBytes(path + "ResourcePacker.pdb")):
+				Assembly.Load(File.ReadAllBytes(path + @"ResourcePacker.dll"));
+
+			var type = assembly.GetType("ResourcePacker.ResourcePackerTask");
+
+			Log.LogError("s2adad" + MSBuildThisFileDirectory);
+
+			//var instance = Activator.CreateInstance(Type);
+			//Log.LogError(Type.GetMethod(nameof(Task.Execute)).Invoke(instance, null).ToString());
+			dynamic instance = Activator.CreateInstance(type);
+			//     into it.
+			instance.BuildEngine = BuildEngine;
+			instance.HostObject = HostObject;
+			//instance.WriteError = new Action<string>(s => BuildEngine.LogErrorEvent(new BuildErrorEventArgs("", "", "", 0, 0, 0, 0, string.Format("Pepita: {0}", s), "", "Pepita")));
+			//instance.SolutionDirectory = @"$(SolutionDir)";
+			//instance.WriteInfo = new Action<string>(s => BuildEngine.LogMessageEvent(new BuildMessageEventArgs(s, "", "Pepita", MessageImportance.High)));
+			//instance.ProjectDirectory = @"$(ProjectDir)";
+			return instance.Execute();
+			//return (bool)Type.GetMethod(nameof(Task.Execute)).Invoke(instance, null);
+			//   } 
+			//    catch (Exception e) 
+			//   {
+			//     Log.LogError(e.Message); 
+			//    }     
+		}
+
+	}
+}
